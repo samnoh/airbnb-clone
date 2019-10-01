0b9a0e4825dbee5c0b4eb5b465fdf37b002c7c58
Sam Noh <samnoh21@outlook.com>
2019-10-01 15:11:49 +1300

    Update reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from core import models as core_models
+from users import models as user_models
+from rooms import models as room_models
 
-# Create your models here.
+
+class Review(core_models.AbstractTimeStampModel):
+    """
+    Review Model Definition
+    """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room = models.ForeignKey(room_models.Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.review} - {self.room}"
