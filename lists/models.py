e62a0a4ab2a30e983321bb11aba55ba9d1679ff6
Sam Noh <samnoh21@outlook.com>
2019-10-01 15:18:01 +1300

    Update lists app

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from core import models as core_models
+from users import models as user_models
+from rooms import models as room_models
 
-# Create your models here.
+
+class List(core_models.AbstractTimeStampModel):
+    """
+    List Model Definition
+    """
+
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    rooms = models.ManyToManyField(room_models.Room, blank=True)
+
+    def __str__(self):
+        return self.name
