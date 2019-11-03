89ad7e2059619ce1590d5633ee88576d97fb45d0
Sam Noh <samnoh21@outlook.com>
2019-11-03 18:41:31 +1300

    feat(users): add signup

--- users/models.py
+++ users/models.py
@@ -31,6 +31,10 @@ class User(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_ENGLISH
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_USD
+    )
     superhost = models.BooleanField(default=False)
