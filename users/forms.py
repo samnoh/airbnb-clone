6cbf659a6bb45e0be432e922324e3bf330c943e1
Sam Noh <samnoh21@outlook.com>
2019-11-05 13:36:37 +1300

    refactor(users): add UserCreationForm to SignUpForm

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -21,39 +22,7 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.ModelForm):
+class SignUpForm(UserCreationForm):
     """ SignUpForm """
 
-    class Meta:
-        model = models.User
-        fields = ("first_name", "last_name", "email")
-
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists with that email")
-        except models.User.DoesNotExist:
-            return email
-
-    def clean_password2(self):
-        password = self.cleaned_data.get("password")
-        password2 = self.cleaned_data.get("password2")
-
-        if password != password2:
-            raise forms.ValidationError("Password confirmation does not match")
-        else:
-            return password
-
-    def save(self, *args, **kwargs):  # override save method
-        user = super().save(
-            commit=False
-        )  # commit=False -> create but don't save the new instance
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-        user.username = email
-        user.set_password(password)  # encrypt password
-        user.save()
+    username = forms.EmailField(label="Email")
