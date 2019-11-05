46b22d156106a56fa49eea2fe0b23a7e200dea92
Sam Noh <samnoh21@outlook.com>
2019-11-05 14:34:27 +1300

    chore(gulpfile): add del

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,31 @@
+import del from 'del';
 import gulp from 'gulp';
 import sass from 'gulp-sass';
-import autoprefixer from 'autoprefixer';
-import postCss from 'gulp-postcss';
-import csso from 'gulp-csso';
 import nodeSass from 'node-sass';
+import postCss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
 import tailwindcss from 'tailwindcss';
+import csso from 'gulp-csso'; // minifiy CSS
 
-sass.compiler = nodeSass;
+const routes = {
+    css: {
+        src: 'assets/scss/main.scss',
+        dest: 'static/css'
+    }
+};
 
-const css = () =>
-    gulp
-        .src('assets/scss/main.scss')
+const clean = () => del([routes.css.dest]);
+
+const sassCompiler = () => {
+    sass.compiler = nodeSass;
+    const { src, dest } = routes.css;
+
+    return gulp
+        .src(src)
         .pipe(sass().on('error', sass.logError))
         .pipe(postCss([tailwindcss, autoprefixer]))
-        .pipe(csso()) // minifiy CSS
-        .pipe(gulp.dest('static/css'));
+        .pipe(csso())
+        .pipe(gulp.dest(dest));
+};
 
-export default css;
+export const css = gulp.series([clean, sassCompiler]);
